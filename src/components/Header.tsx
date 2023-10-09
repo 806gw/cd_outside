c2660c5dc1c3974776a4ae6882ae04f9310458f3
806gw <shjea4364@naver.com>
2023-10-10 02:47:06 +0900

    git update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,17 +12,23 @@ const Header = () => {
 
   return (
     <header className="header">
-      <form onSubmit={handleSearch}>
-        <input
-          placeholder="갤러리 & 통합검색"
-          type="text"
-          value={searchValue}
-          onChange={(e) => setSearchValue(e.target.value)}
+      <div className="ico-search">
+        <img
+          src="https://o.remove.bg/downloads/c45cb8fb-3327-4ea1-be00-fcd4140fa0e8/image-removebg-preview.png"
+          className="gbsw-ico"
         />
-        <button type="submit">
-          <FaSearch/>
-        </button>
-      </form>
+        <form onSubmit={handleSearch}>
+          <input
+            placeholder="갤러리 & 통합검색"
+            type="text"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+          <button type="submit">
+            <FaSearch />
+          </button>
+        </form>
+      </div>
     </header>
   );
 };
